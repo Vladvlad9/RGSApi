cfc6c6843fd9215fe4c847bccc3378a83b1c70f5
Vlad Paulechka <vlad9.ru@gmail.com>
2023-12-14 14:08:53 +0300

    рассылка всем админам

--- schemas/dialogsSchemas.py
+++ schemas/dialogsSchemas.py
@@ -7,8 +7,8 @@ class DialogSchemas(BaseModel):
     user_id: int = Field(ge=1)
     admin_id: int = Field(default=0)
     is_active: bool = Field(default=False)
-    created_at: datetime = Field(default=datetime.now())
-    updated_at: datetime = Field(default=datetime.now())
+    created_at: datetime
+    updated_at: datetime
     who_closed: str = Field(default="None")
     gradeUser: int = Field(ge=1)
     gradeAdmin: int = Field(ge=1)
